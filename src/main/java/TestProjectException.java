d5940da80b8037117643fed81715b24cfec26f3a
Arina Ielchiieva <arina.yelchiyeva@gmail.com>
2016-02-08 13:32:03 +0200

    Fixed testProjectException constructor

--- src/main/java/TestProjectException.java
+++ src/main/java/TestProjectException.java
@@ -1,6 +1,7 @@
 public class TestProjectException extends Exception {
 
-    public TestProjectException(IllegalArgumentException e) {
+    public TestProjectException(Throwable e) {
         super(e);
     }
+
 }
